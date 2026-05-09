9cf181172a149ff866984a2e8fd0ffbf6d8a3d2e
Sophie <sosomeyer1@web.de>
2026-05-09 16:05:36 +0200

    read csv file

--- src/sales_analyzer/__main__.py
+++ src/sales_analyzer/__main__.py
@@ -1,10 +1,21 @@
 import argparse
+from pathlib import Path
 
 def main() -> None: #typehint, funktion gibt nichts zurück
     parser = argparse.ArgumentParser(description="Sales Analyzer") #parser für den programmaufruf in cli
-    parser.add_argument("--file", required=True,help="Path to CSV file") #default="samples/sales.csv"    args = parser.parse_args()
+    parser.add_argument("--file", required=True, help="Path to CSV file") #default="samples/sales.csv"
+    args = parser.parse_args() #übersetzt argumente in datei
+    path = Path(args.file) #gibt die tatsächliche datei
 
-    print(f"Analyzing file: {args.file}")
+    if not path.exists():
+        print(f"Fehler: Datei '{path}' wurde nicht gefunden.")
+        return
+
+    if not path.suffix == ".csv":
+        print(f"Fehler: '{path}' ist keine CSV-Datei.")
+        return
+
+    print(f"Datei gelesen: {path}")
 
 if __name__ == "__main__": #damit programm nur aus dieser datei ausgeführt wird, nicht beim starten von tests
     main()
